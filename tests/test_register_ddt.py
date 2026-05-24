01b5b6799f3f1c3b1c0192801c4323e3064e5a42
Nita Yulia Rahmani <nitayr2005@gmail.com>
2026-05-24 14:01:12 +0700

    Fix: headless mode for register DDT test

--- tests/test_register_ddt.py
+++ tests/test_register_ddt.py
@@ -3,25 +3,20 @@ import csv
 import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 import time
 
 
 def load_data():
     data = []
-
     file_path = os.path.join(
         os.path.dirname(__file__),
         "../data/register_data.csv"
     )
-
     with open(file_path, newline='') as file:
         reader = csv.DictReader(file)
-
         for row in reader:
             data.append(row)
-
     return data
 
 
@@ -29,62 +24,40 @@ def load_data():
 def test_register(data):
 
     options = Options()
-    driver = webdriver.Chrome(options=options)
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--window-size=1920,1080')
+    options.add_argument('--headless=new')
 
+    driver = webdriver.Chrome(options=options)
     driver.maximize_window()
 
     try:
-
         driver.get(
             "https://practicetestautomation.com/practice-test-login/"
         )
 
         time.sleep(2)
 
-        # input username
-        driver.find_element(
-            By.ID,
-            "username"
-        ).send_keys(data["username"])
-
-        # input password
-        driver.find_element(
-            By.ID,
-            "password"
-        ).send_keys(data["password"])
-
-        # klik login
-        driver.find_element(
-            By.ID,
-            "submit"
-        ).click()
+        driver.find_element(By.ID, "username").send_keys(data["username"])
+        driver.find_element(By.ID, "password").send_keys(data["password"])
+        driver.find_element(By.ID, "submit").click()
 
         time.sleep(2)
 
-        # validasi sederhana
         success = "Logged In Successfully" in driver.page_source
 
-        # expected PASS
         if data["expected"] == "PASS":
             assert success
-
-        # expected FAIL
         else:
             assert not success
 
     except Exception as e:
-
-        # buat folder reports
         os.makedirs("reports", exist_ok=True)
-
-        # nama screenshot
         screenshot_name = f'reports/{data["username"]}_fail.png'
-
-        # simpan screenshot
         driver.save_screenshot(screenshot_name)
-
         print(f"Screenshot disimpan: {screenshot_name}")
-
         raise e
 
     finally:
