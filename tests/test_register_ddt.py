76e409d7336ecc424c54ba39d244d1d1fe89a961
Nita Yulia Rahmani <nitayr2005@gmail.com>
2026-05-26 10:16:53 +0700

    Fix: final fix register ddt headless

--- tests/test_register_ddt.py
+++ tests/test_register_ddt.py
@@ -22,43 +22,33 @@ def load_data():
 
 @pytest.mark.parametrize("data", load_data())
 def test_register(data):
-
     options = Options()
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
-    options.add_argument('--disable-gpu')
-    options.add_argument('--window-size=1920,1080')
-    options.add_argument('--headless=new')
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--window-size=1920,1080")
+    options.add_argument("--headless=new")
+    options.add_argument("--remote-debugging-port=9222")
 
     driver = webdriver.Chrome(options=options)
-    driver.maximize_window()
 
     try:
         driver.get(
             "https://practicetestautomation.com/practice-test-login/"
         )
-
         time.sleep(2)
-
         driver.find_element(By.ID, "username").send_keys(data["username"])
         driver.find_element(By.ID, "password").send_keys(data["password"])
         driver.find_element(By.ID, "submit").click()
-
         time.sleep(2)
-
         success = "Logged In Successfully" in driver.page_source
-
         if data["expected"] == "PASS":
             assert success
         else:
             assert not success
-
     except Exception as e:
         os.makedirs("reports", exist_ok=True)
-        screenshot_name = f'reports/{data["username"]}_fail.png'
-        driver.save_screenshot(screenshot_name)
-        print(f"Screenshot disimpan: {screenshot_name}")
+        driver.save_screenshot(f"reports/{data['username']}_fail.png")
         raise e
-
     finally:
         driver.quit()
